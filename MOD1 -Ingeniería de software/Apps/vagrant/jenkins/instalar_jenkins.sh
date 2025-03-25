1d6868674b59de508b50e0d22e82050caaba7092
richi <richi@res.es>
2025-03-25 22:23:45 +0100

    Vagrantfile(jenkins): agregado MOD1- Apps

---
+++ MOD1 -Ingeniería de software/Apps/vagrant/jenkins/instalar_jenkins.sh
@@ -0,0 +1,8 @@
+      sudo apt update
+      sudo apt install -y openjdk-17-jdk
+      curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+      echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+      sudo apt update
+      sudo apt install -y jenkins
+      sudo systemctl enable jenkins
+      sudo systemctl start jenkins
